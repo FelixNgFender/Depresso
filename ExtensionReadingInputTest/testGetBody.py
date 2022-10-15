8c3ecb703dfcc01644a441728d503941d14acae3
Duc Doan <dtbpkmte@gmail.com>
2022-10-15 06:12:51 -0400

    tmp

--- ExtensionReadingInputTest/testGetBody.py
+++ ExtensionReadingInputTest/testGetBody.py
@@ -13,10 +13,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-import js2py
-
-
-
 
 def getIdFromUrl():
     with open('the-zen-of-python.txt') as f:
